817614c4d72f736bda362111d0e39b644abf3052
rlokesh12 <rlokesh121511@gmail.com>
2019-07-29 13:13:47 +0530

    First Commit

---
+++ src/wohoo2/MathCalc.java
@@ -0,0 +1,47 @@
+package wohoo2;
+
+public class MathCalc {
+	
+	public MathCalc() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public int add(int x, int y) {
+		return x+y;
+	}
+	
+	public int subtract(int x, int y) {
+		return x-y;
+	}
+	
+	public int multiply(int x, int y) {
+		return x*y;
+	}
+	
+	public double divide(int x, int y) {
+		double xd = x;
+		double yd = y;
+		double result = 0;
+		try {
+			result = xd/yd;
+		} catch (ArithmeticException e) {
+			System.out.println("y is zero");
+		}
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		MathCalc mathCalc = new MathCalc();
+		int a,s,m;
+		double d;
+		int x,y;
+		x = 10;
+		y = 0;
+		a = mathCalc.add(x, y);
+		s = mathCalc.subtract(x, y);
+		m = mathCalc.multiply(x, y);
+		d = mathCalc.divide(x, y);
+		System.out.println(a+" "+s+" "+m+" "+d);
+	}
+	
+}
